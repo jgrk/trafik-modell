ea97d04ab52100f43444e7f8e3485d06f6391ff4
Hanna Nordström <hkno@kth.se>
2025-01-22 18:14:10 +0100

    Animering outline

--- traffic_template.py
+++ traffic_template.py
@@ -3,8 +3,7 @@
 # Template for traffic simulation
 # BH, MP 2021-11-15, latest version 2024-11-08.
 
-"""gi
-    TJAAAAAAbaaaaaaa
+"""
     This template is used as backbone for the traffic simulations.
     Its structure resembles the one of the pendulum project, that is you have:
     (a) a class containing the state of the system and its parameters
@@ -14,13 +13,14 @@
     You are asked to implement the propagation rule(s) corresponding to the traffic model(s) of the project.
 """
 
-from scipy.optimize import curve_fit
+#from scipy.optimize import curve_fit
 import math
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy.random as rng
 import numpy as np
-from scipy.stats import sem
+#from scipy.stats import sem
+
 
 
 import matplotlib
@@ -28,7 +28,7 @@ import matplotlib
 class Cars:
     """Class for the state of a number of cars"""
 
-    def __init__(self, numCars=5, roadLength=50, v0=1, numLanes=2):
+    def __init__(self, numCars=5, roadLength=50, v0=1, numLanes=3):
         self.numCars = numCars
         self.roadLength = roadLength
         self.t = 0
@@ -36,23 +36,15 @@ class Cars:
         self.v = []
         self.c = []
         self.lanes = []
+        self.numLanes = numLanes  # Antalet körfält
 
         for i in range(numCars):
-            # Set the initial position for each car such that cars are evenly spaced.
+            # Placera bilar jämnt fördelade längs vägen
             self.x.append(i * (roadLength // numCars))
-            self.v.append(v0)  # the speed of the cars
-            self.c.append(i)  # the color of the cars (for drawing)
-            if numLanes == 1:
-                self.lanes.append(1)
-            elif numLanes == 2:
-                randint = rng.randint(1, 100)
-                if randint < 10:
-                    randint = 2
-                else:
-                    randint = 1
-                self.lanes.append(randint)
-            else:
-                self.lanes.append(rng.randint(1, numLanes - 1))
+            self.v.append(v0)  # Hastighet
+            self.c.append(i)  # Färger för visualisering
+            # Tilldela körfält slumpmässigt
+            self.lanes.append(rng.randint(1, numLanes + 1))
 
 
     # Function for computing x-distances
@@ -115,69 +107,81 @@ class ConstantPropagator(BasePropagator):
 
 class MyPropagator(BasePropagator):
 
-    def __init__(self, vmax, p):
-        BasePropagator.__init__(self)
-        self.vmax = vmax
-        self.p = p
+    def __init__(self, vmax, p, safety_distance):
+        super().__init__()
+        self.vmax = vmax  # Maximal hastighet
+        self.p = p  # Sannolikhet för att bromsa slumpmässigt
+        self.safety_distance = safety_distance  # Minsta avstånd till bilen framför
 
     def timestep(self, cars, obs):
         for i in range(cars.numCars):
-            # Determine the distance to the next car
+            # Beräkna avstånd till nästa bil
             d = cars.distance(i)
 
-            # Rule 2: Deceleration - Slow down to avoid collision if the cars are in the same lane
-            if d <= cars.v[i] and cars.lanes[i] == cars.lanes[i+1]:
-                if cars.laneOccupied(car_idx=i, lane_idx=cars.lanes[i]+1) or cars.lanes[i] == cars.numLanes: # Slow down if left lane occupied or does not exist
-                    cars.v[i] = d - 1
-                else: # if overtaking available increase lane number and increase speed
-                    cars.lanes[i] += 1
-                    cars.v[i] += 1
+            # Omkörning: Kontrollera om bilen kan byta till vänster körfält
+            if d <= self.safety_distance and cars.lanes[i] < cars.numLanes:
+                if not cars.laneOccupied(car_idx=i, lane_idx=cars.lanes[i] + 1):
+                    cars.lanes[i] += 1  # Byt till vänster körfält
+                    continue
 
-            if cars.lanes[i] == 1:
-                # Rule 1: Acceleration - Increase speed if possible
-                if cars.v[i] < self.vmax:
-                    cars.v[i] += 1
+            # Återvänd till höger körfält om möjligt
+            if cars.lanes[i] > 1 and not cars.laneOccupied(car_idx=i, lane_idx=cars.lanes[i] - 1):
+                cars.lanes[i] -= 1
 
-                if cars.v[i] > self.vmax:
-                    cars.v[i] -= 1
+            # Acceleration: Öka hastigheten om det är säkert
+            if d > cars.v[i] and cars.v[i] < self.vmax:
+                cars.v[i] += 1
 
-            # rule for middle lane
-            if cars.lanes[i] > 1:
-                if cars.v[i] < self.vmax + cars.lanes[i] - 1:
-                    cars.v[i] += 1
+            # Deceleration: Sänk hastigheten om det är för trångt
+            if d <= self.safety_distance:
+                cars.v[i] = d - 1
 
-                # return to inner lane if possible
-                if cars.laneOccupied(car_idx=i, lane_idx=cars.lanes[i]-1) == False:
-                    cars.lanes[i] -= 1
+            # Randomisering: Bromsa slumpmässigt
+            if rng.rand() < self.p:
+                cars.v[i] = max(0, cars.v[i] - 1)
 
-            # Rule 3: Randomization - Randomly slow down
-            if cars.v[i] > 0 and rng.rand() < self.p:
-                    cars.v[i] -= 1
-
-            # Update position
+            # Uppdatera position
             cars.x[i] = (cars.x[i] + cars.v[i]) % cars.roadLength
 
-        # Update time
+        # Uppdatera tid
         cars.t += 1
 
-        # Calculate flow rate (number of cars passing a point per time step)
+        # Beräkna flöde (antal bilar som passerar per tidsenhet)
         return sum(cars.v) / cars.roadLength
+
 
 
 ############################################################################################
 
 
 def draw_cars(cars, cars_drawing):
-    """Used later on to generate the animation"""
+    """Ritar bilarna och markerar radierna med tunna sträckade linjer."""
     theta = []
     r = []
 
-    for position in cars.x:
-        # Convert to radians for plotting only (do not use radians for the simulation!)
-        theta.append(position * 2 * math.pi / cars.roadLength)
-        r.append(1)
+    # Radier för körfälten
+    lane_radii = {1: 0.6, 2: 0.9, 3: 1.2}
 
+    # Rita de sträckade linjerna för varje radie
+    for radius in lane_radii.values():
+        cars_drawing.plot(
+            np.linspace(0, 2 * math.pi, 100),  # Vinklar (0 till 2π)
+            [radius] * 100,                   # Samma radie över hela cirkeln
+            linestyle="--",                   # Sträckad linje
+            linewidth=0.5,                    # Tunn linje
+            color="gray",                     # Grå färg
+        )
+
+    # Räkna ut bilarnas positioner och radier
+    for position, lane in zip(cars.x, cars.lanes):
+        # Konvertera position till radianer
+        theta.append(position * 2 * math.pi / cars.roadLength)
+        # Tilldela radie beroende på körfält
+        r.append(lane_radii[lane])
+
+    # Rita bilarna
     return cars_drawing.scatter(theta, r, c=cars.c, cmap="hsv")
+
 
 
 def animate(framenr, cars, obs, propagator, road_drawing, stepsperframe):
@@ -276,5 +280,15 @@ class Simulation:
 
         self.plot_observables(title)
         self.plot_positions_vs_time(title + "_positions")
+
+def main():
+    cars = Cars(numCars=20, roadLength=100, v0=0.1, numLanes=3)
+    simulation = Simulation(cars)
+    propagator = MyPropagator(vmax=1, p=0.1, safety_distance=3)
+    simulation.run_animate(propagator, numsteps=200)
+
+
+if __name__ == "__main__":  
+    main()
 
 
